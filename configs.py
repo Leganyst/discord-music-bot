297a768059ab978f7aed0f483b50a49abe2aa2d6
Legan <legan.16@rambler.ru>
2023-04-15 20:20:19 +0300

    Edit configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,4 @@
 login = input('Input your login for VK: ')
 password = input('Input your password for VK: ')
 
-
-token_bot_discord = 'MTA5NjEyMjkxMzg3NjM1MzE1NA.GhC52W.--TBKa0ycdaly8a5Q9pZrvuU0hunpMqfPnZr6k'
+token_bot_discord = 'your discord token bot'
