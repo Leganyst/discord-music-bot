e8e16a893b123dc3d4e28a5a94ef6bf3ece716e8
Legan <legan.16@rambler.ru>
2023-04-15 20:16:15 +0300

    Create requirements.txt

--- configs.py
+++ configs.py
@@ -2,4 +2,4 @@ login = input('Input your login for VK: ')
 password = input('Input your password for VK: ')
 
 
-token_bot_discord = 'MTA5NjEyMjkxMzg3NjM1MzE1NA.GT3cOo.C8R1hkOfjcecYTR92snA_83DJ4se9ZIet8SmAI'
+token_bot_discord = 'MTA5NjEyMjkxMzg3NjM1MzE1NA.GhC52W.--TBKa0ycdaly8a5Q9pZrvuU0hunpMqfPnZr6k'
