4306c1b297f960be6b948ed92044995e08f42250
Legan <legan.16@rambler.ru>
2023-04-15 20:01:51 +0300

    Fix import command

--- configs.py
+++ configs.py
@@ -2,4 +2,4 @@ login = input('Input your login for VK: ')
 password = input('Input your password for VK: ')
 
 
-token_bot_discord = 'Token your discord bot'
+token_bot_discord = 'MTA5NjEyMjkxMzg3NjM1MzE1NA.GT3cOo.C8R1hkOfjcecYTR92snA_83DJ4se9ZIet8SmAI'
--- main_bot.py
+++ main_bot.py
@@ -1,4 +1,5 @@
 from configs import token_bot_discord
 from discord_part.botInit import bot
+from discord_part import musicCommands
 # Запускаем бота с вашим токеном дискорда
 bot.run(token_bot_discord)
